3a364cba18855dd2721b292a29d45d0bb7334c6b
scriptlver <sloowlaah1@gmail.com>
2025-06-28 11:15:00 -0300

    Carrinhoo

---
+++ src/services/cart.js
@@ -0,0 +1,16 @@
+async function addItem(userCart){ //funcao para adicionar item no carrinho
+
+}
+
+async function deleteItem(userCart, name){ //funcao para delte um item do carrinho a partir do seu nome
+
+}  
+
+async function removeItem (userCart, index){ //funcao para diminuir um item do carrinho 
+
+}
+
+async function calculateTotal(userCart){ //funcao para calcular o total do carrinho
+
+}
+
